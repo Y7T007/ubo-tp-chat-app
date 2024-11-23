48098fcfe54cd04f4bb5f2c93448bd1033d566a5
Y7T007 <yassir.wahid@etu.uae.ac.ma>
2024-11-23 15:20:08 +0100

    Updaing the upload-image.js to not be runtime edge

--- api/upload-image.js
+++ api/upload-image.js
@@ -1,9 +1,7 @@
 import { put } from "@vercel/blob";
-import { getConnecterUser, unauthorizedResponse } from "../lib/session";
+import {Redis} from "@upstash/redis";
+const redis = Redis.fromEnv();
 
-export const config = {
-    runtime: 'edge',
-};
 
 export default async function handler(request) {
     try {
@@ -43,4 +41,27 @@ export default async function handler(request) {
             headers: { 'content-type': 'application/json' },
         });
     }
+}
+
+export async function getConnecterUser(request) {
+    let token = new Headers(request.headers).get('Authorization');
+    if (!token) {
+        return null;
+    } else {
+        token = token.replace("Bearer ", "");
+    }
+    console.log("checking " + token);
+    const user = await redis.get(token);
+    if (user) {
+        console.log("Got user : " + user.username);
+    }
+    return user;
+}
+
+export function unauthorizedResponse() {
+    const error = { code: "UNAUTHORIZED", message: "Session expired" };
+    return new Response(JSON.stringify(error), {
+        status: 401,
+        headers: { 'content-type': 'application/json' },
+    });
 }
