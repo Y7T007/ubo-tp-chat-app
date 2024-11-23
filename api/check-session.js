454b587ae0d48da1577226695324a6ca06a26a59
Y7T007 <yassir.wahid@etu.uae.ac.ma>
2024-11-23 16:01:31 +0100

    Updating check-session

--- api/check-session.js
+++ api/check-session.js
@@ -1,8 +1,7 @@
-import { checkSession, unauthorizedResponse } from "../lib/session";
+import { Redis } from '@upstash/redis';
 
-export const config = {
-    runtime: 'edge',
-};
+const redis = Redis.fromEnv();
+
 
 export default async function handler(request) {
     try {
@@ -22,4 +21,31 @@ export default async function handler(request) {
             headers: { 'content-type': 'application/json' },
         });
     }
+}
+
+export async function checkSession(request) {
+    const user = await getConnecterUser(request);
+    return user !== null;
+}
+
+export function unauthorizedResponse() {
+    const error = { code: "UNAUTHORIZED", message: "Session expired" };
+    return new Response(JSON.stringify(error), {
+        status: 401,
+        headers: { 'content-type': 'application/json' },
+    });
+}
+export async function getConnecterUser(request) {
+    let token = new Headers(request.headers).get('Authorization');
+    if (!token) {
+        return null;
+    } else {
+        token = token.replace("Bearer ", "");
+    }
+    console.log("checking " + token);
+    const user = await redis.get(token);
+    if (user) {
+        console.log("Got user : " + user.username);
+    }
+    return user;
 }
